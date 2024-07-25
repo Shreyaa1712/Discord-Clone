c5577eec388470c5c98d61838103dc03af51483d
Shreyaa <shreyaa.jalan17@gmail.com>
2024-07-25 15:55:19 +0530

    "Added FileUpload component, updated middleware and next.config, and installed new dependencies"

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -3,6 +3,7 @@
 import * as z from "zod";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {useForm} from "react-hook-form";
+import { useEffect, useState} from "react";
 
 import {
     Dialog,
@@ -22,7 +23,7 @@ import {
   } from "@/components/ui/form";
   import { Button } from "@/components/ui/button";
   import { Input } from "@/components/ui/input";
-  import { useEffect, useState} from "react";
+  import { FileUpload } from "@/components/file-upload";
   const formSchema = z.object({
     name: z.string().min(1,{
         message: "Server name is required."
@@ -70,13 +71,26 @@ export const InitialModel =()=> {
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <div className="space-y-8 px-6">
             <div className="flex items-center justify-center text-center">
-                TODO: Image Upload 
+                <FormField 
+                control={form.control}
+                name="imageUrl"
+                render={({field})=>(
+                  <FormItem>
+                    <FormControl>
+                    <FileUpload
+                    endpoint="serverImage"
+                    value={field.value}
+                    onChange={field.onChange}
+                  />
+                    </FormControl>
+                  </FormItem>
+                )}
+                  />
             </div>
-             
             <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
+             control={form.control}
+             name="name"
+             render={({ field }) => (
             <FormItem>
               <FormLabel className="uppercase text-xs font-bold text-zinc-500 
               dark:text-secondary/70">
